553b539faba6ef8f274d1bf7e2670c1c7bb5b234
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-06-19 17:47:35 +0200

    added ranks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import json
 
 from discord_slash import SlashCommand
 from discord_slash import SlashContext
@@ -7,7 +8,6 @@ import discord
 from discord.ext.commands import Bot
 
 import os
-import time
 
 intents = discord.Intents.default()
 intents.members = True
@@ -20,23 +20,43 @@ TOKEN = os.getenv("TOKEN")
 
 # video: https://youtu.be/CE0noW3flT8
 
+whitelist = open("whitelist.json")
+whitelist = json.load(whitelist)
+
 @bot.event
 async def on_ready():
     activity = discord.Game(name="finn abfucken")
     await bot.change_presence(status=discord.Status.online, activity=activity)
+
+    
+
     print("ready um Finn abzufucken")
 
-    @slash.slash(name="abfuck", description="hello!")
-    async def abfuck(self, ctx: SlashContext):
-        await member.send("hello")
+def checkMember(member):
+    for i in whitelist["ultra"]:
+        if i == member:
+            return "ultra"
+        else:
+            for i in whitelist["protectionPremium"]:
+                return "premium"
+            else:
+                for i in whitelist["protectionLow"]:
+                    return "low"
+                else:
+                    return False
 
-    @bot.command()
-    async def abfuck(ctx, member: discord.Member, msg):
+    
+@bot.command()
+async def abfuck(ctx, member: discord.Member, msg):
+    member = str(member)
+    if checkMember(member) == "ultra":
+        await ctx.send(member + " has ultra rank")
+    else: 
         i=0
         while True:
             i=i+1
             await member.send(msg + "\ncount: " + str(i))
-            print("send message to " + str(member) + " \nmessage: " + msg + "\n count: " + str(i) + "\n------------------")
+            print("send message to " + member + " \nmessage: " + msg + "\n count: " + str(i) + "\n------------------")
             time.sleep(3)
 
 bot.run(TOKEN)
